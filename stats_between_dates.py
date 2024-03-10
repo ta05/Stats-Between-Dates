3b6e060ec474e1ad8a2ca0bfbe3d1f89e24c1b7f
Tolu Alimi <talimi2000@gmail.com>
2024-03-10 05:05:17 -0500

    Added source code

---
+++ stats_between_dates.py
@@ -0,0 +1,92 @@
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import cumestatsplayer, leaguegamefinder
+from dateutil.parser import parse
+from datetime import datetime
+import pandas as pd
+import json
+import time
+import requests
+
+# Retry Wrapper
+def retry(func, retries: int=3):
+    def retry_wrapper(*args, **kwargs) -> None:
+        attempts = 0
+        while attempts < retries:
+            try:
+                return func(*args, **kwargs)
+            except requests.exceptions.RequestException as e:
+                print(e)
+                time.sleep(30)
+                attempts += 1
+            
+    return retry_wrapper
+
+
+def getSeasons(start_date: datetime, end_date: datetime) -> list[str]:
+    return [f'{year}-{str(year + 1)[-2:]}' for year in range(start_date.year, end_date.year + 1)]
+
+
+def getPlayerId(name: str) -> str:
+    return players.find_players_by_full_name(name)[0]['id']
+
+
+def getPerGameMetrics(playerID: str, start_date: datetime, end_date: datetime) -> pd.DataFrame:
+
+    @retry
+    def getGames(playerID: str, start_date: datetime, end_date: datetime) -> list[str]:
+        games = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation="P", season_type_nullable="Regular Season", player_id_nullable=playerID).get_normalized_json()
+        games = json.loads(games)['LeagueGameFinderResults']
+        return [game['GAME_ID'] for game in games if start_date <= parse(game['GAME_DATE']) and end_date >= parse(game['GAME_DATE'])]
+
+
+    @retry
+    def getGameStats(playerID: str, gameIDs: list[str]) -> pd.DataFrame:
+        gameStats = cumestatsplayer.CumeStatsPlayer(player_id=playerID, game_ids=gameIDs, league_id='00', season_type_all_star="Regular Season").get_normalized_json()
+        gameStats = pd.DataFrame(json.loads(gameStats)['GameByGameStats'])
+
+        return gameStats
+
+
+    gameIDs = getGames(playerID=playerID, start_date=start_date, end_date=end_date)
+
+    data = pd.DataFrame()
+    for id in gameIDs:
+        time.sleep(1)
+        data = pd.concat([data, getGameStats(playerID, [id])])
+
+    return data
+
+
+def getTotalStats(gameStats: pd.DataFrame) -> dict:
+    totalStats = gameStats[['GP', 'FG', 'FGA', 'PTS', 'FG3', 'FG3A', 'AST', 'OFF_REB', 'DEF_REB', 'TOT_REB', 'STL', 'BLK', 'TURNOVERS']].sum().rename({'TURNOVERS': 'TO'}).to_dict()
+
+    
+    totalStats['FG%'] = totalStats['FG'] / totalStats['FGA']
+    totalStats['3PTS'] = totalStats['FG3'] * 3
+    totalStats['FG3%'] = totalStats['FG3'] / totalStats['FG3A']
+
+    totalStats['2PTS'] = totalStats['PTS'] - totalStats['3PTS']
+    totalStats['FG2'] = totalStats['FG'] - totalStats['FG3']
+    totalStats['FG2A'] = totalStats['FGA'] - totalStats['FG3A']
+    totalStats['FG2%'] = totalStats['FG2'] / totalStats['FG2A']
+
+    return totalStats
+
+
+def main():
+    name = input("Enter Player Name: ")
+    playerID = getPlayerId(name)
+
+    start_date = parse(input("Enter Start Date (MM/DD/YYYY): "))
+    end_date = parse(input("Enter End Date (MM/DD/YYYY): "))
+
+    gameStats = getPerGameMetrics(playerID, start_date, end_date)
+
+    totalStats = getTotalStats(gameStats)
+
+    print(totalStats)
+
+
+if __name__ == "__main__":
+    main()
+
